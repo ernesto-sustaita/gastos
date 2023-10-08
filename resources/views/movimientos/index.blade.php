f4d24c69ae732390c3e3daea691b98656141323a
Ernesto Sustaita <ernesto.sustaita@yahoo.fr>
2023-10-08 15:41:16 -0400

    Actualización de modulo movimientos

--- resources/views/movimientos/index.blade.php
+++ resources/views/movimientos/index.blade.php
@@ -3,30 +3,34 @@
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <form method="POST" action="{{ route('movimientos.store') }}">
             @csrf
-            <select name="cuenta"
+            <select name="cuenta_id"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
                 <option disabled="disabled" selected="selected">Seleccionar cuenta</option>
                 @foreach ($cuentas as $cuenta)
                     <option value="{{ $cuenta->id }}">{{ $cuenta->nombre }} - {{ $cuenta->tipo }}</option>
                 @endforeach
             </select>
-            <input type="text"
-                name="nombre"
-                placeholder="{{ __('Nombre de la cuenta') }}"
-                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
-            />
             @csrf
             <select 
                 name="tipo"
                 class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm">
-                <option disabled="disabled" selected="selected">Seleccionar tipo de cuenta</option>
-                <option value="efectivo">Efectivo</option>
-                <option value="debito">Débito</option>
-                <option value="credito">Crédito</option>
+                <option disabled="disabled" selected="selected">Seleccionar tipo de movimiento</option>
+                <option value="ingreso">Ingreso</option>
+                <option value="egreso">Egreso</option>
             </select>
-            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
+            @csrf
+            <input type="number"
+                step="0.1"
+                name="cantidad"
+                placeholder="{{ __('Cantidad') }}"
+                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+            />
+            @csrf
+            <input type="date" id="start" name="fecha" 
+                class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" />
+            <x-input-error :messages="$errors->get('cantidad')" class="mt-2" />
             <x-input-error :messages="$errors->get('tipo')" class="mt-2" />
-            <x-primary-button class="mt-4">{{ __('Agregar cuenta') }}</x-primary-button>
+            <x-primary-button class="mt-4">{{ __('Agregar movimiento') }}</x-primary-button>
         </form>
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             <table>
